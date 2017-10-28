b59000d8fb68dead01d1008de13108f082c9499d
ikt2y <ikd618@gmail.com>
2017-10-28 12:27:02 +0900

    :sparkles: Add based Mapkit

---
+++ TakeU/TakeU/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  TakeU
+//
+//  Created by ikt2y on 2017/10/28.
+//  Copyright © 2017年 shibugame. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    // MapView.
+    var myMapView : MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // MapViewの生成.
+        myMapView = MKMapView()
+        
+        // MapViewのサイズを画面全体に.
+        myMapView.frame = self.view.bounds
+        
+        // Delegateを設定.
+        myMapView.delegate = self
+        
+        // MapViewをViewに追加.
+        self.view.addSubview(myMapView)
+        
+        // 中心点の緯度経度.
+        let myLat: CLLocationDegrees = 37.506804
+        let myLon: CLLocationDegrees = 139.930531
+        let myCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLat, myLon)
+        
+        // 縮尺.
+        let myLatDist : CLLocationDistance = 100
+        let myLonDist : CLLocationDistance = 100
+        
+        // Regionを作成.
+        let myRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(myCoordinate, myLatDist, myLonDist);
+        
+        // MapViewに反映.
+        myMapView.setRegion(myRegion, animated: true)
+        
+    }
+    
+    // Regionが変更された時に呼び出されるメソッド.
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        print("regionDidChangeAnimated")
+    }
+}
